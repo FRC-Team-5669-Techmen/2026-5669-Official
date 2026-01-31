b5aefbf76b6561f70920d4f26dce70bb8172922e
aperson78 <jhonenrh@gmail.com>
2026-01-31 15:59:05 -0800

    added turret subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,12 +3,8 @@
 // the WPILib BSD license file in the root directory of this project.
 // Muhammad Nabeel
 //Lukas Deusch 
-
-
-
 package frc.robot;
     
-
 import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
@@ -16,6 +12,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.commands.RunShooterCommand;
+import frc.robot.commands.TurretAlign;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -25,6 +22,8 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -43,6 +42,8 @@ public class RobotContainer {
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
     public final ShooterSubsystem shooter = new ShooterSubsystem();
+    public final TurretSubsystem sixseven = new TurretSubsystem();
+    public final LimelightSubsystem rizz = new LimelightSubsystem();
 
     public RobotContainer() {
         configureBindings();
@@ -68,6 +69,7 @@ public class RobotContainer {
         );
 
         joystick.a().whileTrue(new RunShooterCommand(shooter, 5000));
+        joystick.x().whileTrue(new TurretAlign(rizz, sixseven));
         //joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
         joystick.b().whileTrue(drivetrain.applyRequest(() ->
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
